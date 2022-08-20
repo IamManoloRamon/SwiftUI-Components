224c531ea54752b7274c4824671f06c579776232
Manolo Ramon Manatlao <manolo-ramon@outlook.com>
2022-08-21 01:03:01 +0400

    another commit

---
+++ ComponenttUI/ComponenttUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponenttUIApp.swift
+//  ComponenttUI
+//
+//  Created by Manolo Ramon Manatlao on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ComponenttUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
